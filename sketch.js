b358223a5d7fa15e402797c27e18005cd3c9e81f
Bronson Leiro <bkl20a@fsu.edu>
2022-10-24 11:34:56 -0400

    Cubye

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+
+
+
+function preload() {
+  // Load model with normalise parameter set to true
+  cubye = loadModel('Models/cubye.obj', true);
+  cubye2 = loadModel('Models/cubye2.obj', true );
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000, WEBGL);
+  describe('Vertically rotating 3-d teapot with red, green and blue gradient.');
 }
 
 function draw() {
-  background(125);
+  background(200);
+  
+  scale(0.2); // Scaled to make model fit into canvas
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  normalMaterial(); // For effect
+  model(cubye);
+  translate (30,30)
+  model(cubye2);
+  translate (40,600)
+  model(cubye2)
+
+  //have a slider with multiple files depicting multiple extrudes
+  
 }
+
