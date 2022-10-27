4d1b87daf3b64e1f71a8ca962cdb1b6c7aa1d515
Durban Grove <bronsonleiro@gmail.com>
2022-10-26 20:31:07 -0400

    Interface Abstracted

--- sketch.js
+++ sketch.js
@@ -5,27 +5,61 @@ function preload() {
   // Load model with normalise parameter set to true
   cubye = loadModel('Models/cubye.obj', true);
   cubye2 = loadModel('Models/cubye2.obj', true );
+  slider = createSlider(0,9,100);
+  slider.position(100,100);
+  slider.style('width','80px');
 }
 
 function setup() {
-  createCanvas(1000, 1000, WEBGL);
-  describe('Vertically rotating 3-d teapot with red, green and blue gradient.');
+  createCanvas(500, 500, WEBGL);
+ 
 }
 
 function draw() {
   background(200);
+  fill("Honeydew")
+  stroke("White")
+  rect(-80, -40, 330, 290,)
+  fill("midnightblue")
+  stroke("honeydew")
+  rect(-030,-120,40,40)
+  fill("midnightblue")
+  stroke("Honeydew")
+  rect(40,-120,40,40)
+  fill("midnightblue")
+  rect(40,-200,40,40)
+  fill("midnightblue")
+  rect(-030,-200,40,40)
+  fill("antiquewhite")
+  rect(-200,-60,40,5)
+  fill("black")
+  rect(-185,-80,8,80)
+  fill("antiquewhite")
+  rect(-200,40,40,5)
+  fill("black")
+  rect(-185,30,8,80)
+  fill(200,200,40)
+  rect(-200, -180, 100,10)
+  fill(200,200,200)
+  rect(-200,-180,20,10)
+  fill(100,50,100)
+  rect(130,-200, 100,120)
+  fill("blue")
+  stroke("honeydew")
+  rect(-030,-120,20,20)
+  fill("blue")
+  stroke("Honeydew")
+  rect(40,-120,20,20)
+  fill("blue")
+  rect(40,-200,20,20)
+  fill("blue")
+  rect(-030,-200,20,20)
+  let val = slider.value();
   
-  scale(0.2); // Scaled to make model fit into canvas
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  normalMaterial(); // For effect
-  model(cubye);
-  translate (30,30)
-  model(cubye2);
-  translate (40,600)
-  model(cubye2)
 
+
+ 
   //have a slider with multiple files depicting multiple extrudes
-  
+
 }
 
