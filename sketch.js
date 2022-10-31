5d3386d85104693d00dc94edc55e1598682dae09
Onaik <bronsonleiro@gmail.com>
2022-10-31 00:50:47 -0400

    Interactivity Reduction

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 
-
+var widthSlider
 
 function preload() {
   // Load model with normalise parameter set to true
   cubye = loadModel('Models/cubye.obj', true);
   cubye2 = loadModel('Models/cubye2.obj', true );
-  slider = createSlider(0,9,100);
+  slider = createSlider(0,100,40);
   slider.position(100,100);
   slider.style('width','80px');
+  widthSlider = createSlider (3,100,40)
+  widthSlider.position(40,40)
+  
+
 }
 
 function setup() {
@@ -17,10 +21,11 @@ function setup() {
 
 function draw() {
   background(200);
+
   fill("Honeydew")
   stroke("White")
   rect(-80, -40, 330, 290,)
-  fill("midnightblue")
+  fill("midnightblue")                                       
   stroke("honeydew")
   rect(-030,-120,40,40)
   fill("midnightblue")
@@ -44,7 +49,7 @@ function draw() {
   rect(-200,-180,20,10)
   fill(100,50,100)
   rect(130,-200, 100,120)
-  fill("blue")
+ /* fill("blue")
   stroke("honeydew")
   rect(-030,-120,20,20)
   fill("blue")
@@ -53,7 +58,13 @@ function draw() {
   fill("blue")
   rect(40,-200,20,20)
   fill("blue")
-  rect(-030,-200,20,20)
+  rect(-030,-200,20,20)*/
+  translate(50,100)
+  cylinder(widthSlider.value(),slider.value())
+
+ 
+  
+
   let val = slider.value();
   
 
